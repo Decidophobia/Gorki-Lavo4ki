cb4fed847407c284a173b3dd6116996f6e016298
Vladislav <semakin91@gmail.com>
2020-11-23 18:45:17 +0300

    get full post for blog page

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -18,7 +18,7 @@ router.post('/signup', async (req, res) => {
 				email: newUser.email,
 				userId: newUser._id
 			}, accessTokenSecret.jwt, {expiresIn: 60 * 60});
-			res.status(200).json({token: `Bearer ${ accessToken }`, name: newUser.name});
+			res.status(200).json({token: `Bearer ${ accessToken }`, name: newUser.name, userId: newUser._id});
 		} else {
 			res.send('User already exists!');
 		}
@@ -38,7 +38,7 @@ router.post('/login', async (req, res) => {
 				email: user.email,
 				userId: user._id
 			}, accessTokenSecret.jwt, {expiresIn: 60 * 60});
-			res.status(200).json({token: `Bearer ${ accessToken }`, name: user.name});
+			res.status(200).json({token: `Bearer ${ accessToken }`, name: user.name, userId: user.id});
 		} else {
 			res.status(401).json({message: 'Username or password incorrect'});
 		}
