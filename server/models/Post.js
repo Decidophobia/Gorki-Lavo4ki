1fa9791e8136299a400094b69565c31769194115
AleksandrKocherga <senior.kocherga@gmail.com>
2020-11-25 12:56:33 +0300

    add address in DB

--- server/models/Post.js
+++ server/models/Post.js
@@ -6,10 +6,11 @@ const Schema = mongoose.Schema;
 const postSchema = Schema({
   authorID: { type: Schema.Types.ObjectId, ref: 'User' },
   coord: [],
-  title: { type: String},
+  title: { type: String },
   description: { type: String },
   photo: { type: String },
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  address: { type: String },
 });
 
 module.exports = mongoose.model('Post', postSchema);
