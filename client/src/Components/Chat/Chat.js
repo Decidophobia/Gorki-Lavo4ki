ceee92acf5923a9d0243b2dc6f16e7ba91c9a966
Alex <Sersurgut88@yandex.ru>
2020-11-26 02:50:39 +0300

    Create string 'Who writes' in chat

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -8,12 +8,14 @@ import { useParams } from 'react-router-dom';
 
 function Chat() {
   const [message, setMessage] = useState("");
+  const [write, setWrite] = useState("");
   const dispatch = useDispatch();
   const chat = useSelector((store) => store.chat.messages);
   const username = JSON.parse(localStorage.getItem("name"));
   const room = useParams().coordId
   const messageText = (e) => {
     setMessage(e.target.value);
+    // setWrite('Печатает')
   };
   const sendMessage = () => {
     dispatch(sendMessageAC(message));
@@ -24,15 +26,30 @@ function Chat() {
   const onKeyDown = (e) => {
     if (e.keyCode == 13) sendMessage();
   };
-
   useEffect(() => {
     socket.on("NEW_MESSAGE:CLIENT", (message) => {
       dispatch(sendMessageAC(message));
     });
     socket.emit("CONNECT_ROOM", room);
+        
   }, []);
+
+    useEffect(() => {
+
+    socket.emit("WRITE_MESSAGE", username, room)
+    socket.on("WRITE_MESSAGE:CLIENT", (user) => {
+    setWrite(`${user} печатает...`)
+    setTimeout(() => {
+      setWrite('')
+    }, 2000);
+ 
+    console.log(user);
+    });
+
+  }, [message]);
   return (
     <div className={styles.chatContainer}>
+      <div>{write}</div>
       <div className={styles.chatField}>
         {chat &&
           chat.map((oneMes, i) => (
