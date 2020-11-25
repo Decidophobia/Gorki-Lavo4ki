c02a732809f87427a7870b2a8b0d4e9ae154ded0
Alex <Sersurgut88@yandex.ru>
2020-11-25 11:14:46 +0300

    change model user

--- server/models/User.js
+++ server/models/User.js
@@ -8,7 +8,9 @@ const userShema = Schema({
   name: {type: String, require: true},
   phone: {type: Number, require: true, unique: true},
   room: {type: Schema.Types.ObjectId, ref: 'Chat' },
-  postRef: [ { type: Schema.Types.ObjectId, ref: 'Post' }]
+  postRef: [ { type: Schema.Types.ObjectId, ref: 'Post' }],
+  status: {type:Boolean, default:false},
+  image: {type:String, default:"https://lh3.googleusercontent.com/q2k2Mco2k7wcSz_M3Wo0QL_YoMdXiL8FT1X_tqNN2A7PpAyEfTu9aHEQB6lARDBLXw"}
 });
 
 module.exports = mongoose.model("User", userShema);
